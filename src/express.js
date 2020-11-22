2476fd3bc92b2520e56f4735f4554978c810922b
peter heinum <peter.heinum@gmail.com>
2020-11-22 17:18:05 +0100

    added response to request

--- src/express.js
+++ src/express.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
 const { incrementSnus } = require('./mongo')
+const { pipe } = require('ramda')
 
-const isoDate = () => new Date().toISOString()
+const defaultHandler = (req, res) => pipe(
+  fn => fn(),
+  () => res.send('incremented')
+)
 
-app.get('/', (req, res) => {
-  res.send('use /increment')
-})
-
-app.get('/increment', (req, res) => incrementSnus())
+app.get('/increment', (...args) => defaultHandler(...args)(incrementSnus))
  
 app.listen(process.env.PORT || 3000)
